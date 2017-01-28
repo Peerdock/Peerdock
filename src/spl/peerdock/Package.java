e4ae930b88a32b0ecdb7c3788e9b5ef498d743ac
Victor Lourme <spl0it@protonmail.ch>
2017-01-28 10:05:07 +0100

    Added TrustManager for SSL/HTTPS request
    Warning: This is a untested feature.

--- src/spl/peerdock/Package.java
+++ src/spl/peerdock/Package.java
@@ -129,6 +129,29 @@ class Package {
         System.out.println("<+> Fetching package list");
         System.out.println(" |> URL : " + arg);
         String result = "";
+        
+        TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(
+                    java.security.cert.X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(
+                    java.security.cert.X509Certificate[] certs, String authType) {
+                }
+            }
+        };
+
+        // Activate the new trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            System.out.println("<!> Error: " + e.getMessage());
+        }
 
         URL xml = new URL(arg);
         URLConnection yc = xml.openConnection();
